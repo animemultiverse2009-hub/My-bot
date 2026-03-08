d281cac54e30cafdb16f6bf72f268a40c01fd196
verification <animemultiverse2009@gmail.com>
2026-03-08 12:12:52 +0530

    Delete handlers directory

--- handlers/unlockPrize.js
+++
@@ -1,8 +0,0 @@
-const skipQueue = require('./skipQueue');
-
-module.exports = {
-    async execute(interaction, client) {
-        // This is essentially the same as skip queue - recheck invites
-        await skipQueue.execute(interaction, client);
-    }
-};
